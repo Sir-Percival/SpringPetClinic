bd75bc1dd2a1d4a896a400685210d6127850e634
Kacper Walichnowski <kacperw112@wp.pl>
2023-08-25 12:11:18 +0200

    Add BaseEntity

--- PetClinicData/src/main/java/com/project/springpetclinic/models/Person.java
+++ PetClinicData/src/main/java/com/project/springpetclinic/models/Person.java
@@ -1,6 +1,6 @@
 package com.project.springpetclinic.models;
 
-public class Person
+public class Person extends BaseEntity
 {
     private String firstName;
     private String lastName;
--- PetClinicData/src/main/java/com/project/springpetclinic/models/PetType.java
+++ PetClinicData/src/main/java/com/project/springpetclinic/models/PetType.java
@@ -1,6 +1,6 @@
 package com.project.springpetclinic.models;
 
-public class PetType
+public class PetType extends BaseEntity
 {
     private String name;
 
