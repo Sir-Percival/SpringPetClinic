4893a1283a365da5289719e7e8d8036031a718e0
Kacper Walichnowski <kacperw112@wp.pl>
2023-09-07 12:45:24 +0200

    Implement Speciality Map Service

---
+++ PetClinicData/src/main/java/com/project/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.project.springpetclinic.services;
+
+import com.project.springpetclinic.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ PetClinicData/src/main/java/com/project/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.project.springpetclinic.services.map;
+
+import com.project.springpetclinic.models.Speciality;
+import com.project.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
