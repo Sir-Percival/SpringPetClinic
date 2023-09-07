dd5d5a36a1f33de4f9f3a9855ad3d6b959d4f85d
Kacper Walichnowski <kacperw112@wp.pl>
2023-09-07 11:20:31 +0200

    Add Speciality POJO, add it to Vet

---
+++ PetClinicData/src/main/java/com/project/springpetclinic/models/Speciality.java
@@ -0,0 +1,14 @@
+package com.project.springpetclinic.models;
+
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
