8cdfa5064f73d05b85731e285480f4fc28576817
Kacper Walichnowski <kacperw112@wp.pl>
2023-09-14 13:44:39 +0200

    Update Visit with JPA annotations

--- PetClinicData/src/main/java/com/project/springpetclinic/models/Visit.java
+++ PetClinicData/src/main/java/com/project/springpetclinic/models/Visit.java
@@ -1,11 +1,21 @@
 package com.project.springpetclinic.models;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity
 {
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
